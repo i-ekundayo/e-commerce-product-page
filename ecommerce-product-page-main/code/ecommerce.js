ae2ec73aa1cf57bf7fa932f3e28e32a6f3608e4f
i-ekundayo <isekundayo700@gmail.com>
2023-01-14 13:13:54 +0100

    Updated ecommerce
    Worked on the previous and next button

--- ecommerce-product-page-main/code/ecommerce.js
+++ ecommerce-product-page-main/code/ecommerce.js
@@ -37,64 +37,61 @@ const bigImg = document.querySelectorAll('.bigImg');
         
 
 for(let i=0; i<img.length; i++) {
-    let newIndex = i;
     img[i].addEventListener('click', (e) => {
-        let newIndex = i;
         mainImage.src = bigImg[i].src;
-        lightBoxMainImage.src = bigImg[newIndex].src;
 
-        // working on setting the corresponding border and opacity of the thumbnail images
-        function borderChange(){
-            Array.from(lightBoxImg).forEach((im) => {
-                Array.from(lightBoxImage).forEach((el) => {
-                    el.style.border = 'none';
-                    im.style.opacity = '1';
-                })
-                im.addEventListener('mouseenter', (e) => {
-                    im.style.opacity = '0.7';
-                })
-                im.addEventListener('mouseleave', (e) => {
-                    im.style.opacity = '1';
-                })
-            })
-            lightBoxImage[newIndex].style.border = '2px solid hsl(26, 100%, 55%)';
-            lightBoxImg[newIndex].style.opacity = '0.4';
-            lightBoxImg[newIndex].addEventListener('mouseenter', (e) => {
-                lightBoxImg[newIndex].style.opacity = '0.4';
-            })
-            lightBoxImg[newIndex].addEventListener('mouseleave', (e) => {
-                lightBoxImg[newIndex].style.opacity = '0.4';
-            })
-        }
-        borderChange();
+        // // working on setting the corresponding border and opacity of the thumbnail images
+        // function borderChange(){
+        //     Array.from(lightBoxImg).forEach((im) => {
+        //         Array.from(lightBoxImage).forEach((el) => {
+        //             el.style.border = 'none';
+        //             im.style.opacity = '1';
+        //         })
+        //         im.addEventListener('mouseenter', (e) => {
+        //             im.style.opacity = '0.7';
+        //         })
+        //         im.addEventListener('mouseleave', (e) => {
+        //             im.style.opacity = '1';
+        //         })
+        //     })
+        //     lightBoxImage[newIndex].style.border = '2px solid hsl(26, 100%, 55%)';
+        //     lightBoxImg[newIndex].style.opacity = '0.4';
+        //     lightBoxImg[newIndex].addEventListener('mouseenter', (e) => {
+        //         lightBoxImg[newIndex].style.opacity = '0.4';
+        //     })
+        //     lightBoxImg[newIndex].addEventListener('mouseleave', (e) => {
+        //         lightBoxImg[newIndex].style.opacity = '0.4';
+        //     })
+        // }
+        borderChange(i);
 
-        // working on the previous and next button
-        const prev = document.querySelector('.prev');
-        const next = document.querySelector('.next');
-        prev.addEventListener('click', (e) => {
-            newIndex--;
-            if(newIndex < 0){
-                newIndex = bigImg.length-1;
-                lightBoxMainImage.src = bigImg[newIndex].src;
-                borderChange();
-            }
-            else{
-                lightBoxMainImage.src = bigImg[newIndex].src;
-                borderChange();
-            }
-        })
-        next.addEventListener('click', (e) => {
-            newIndex++;
-            if(newIndex > bigImg.length-1){
-                newIndex = 0;
-                lightBoxMainImage.src = bigImg[newIndex].src;
-                borderChange();
-            }
-            else{
-                lightBoxMainImage.src = bigImg[newIndex].src;
-                borderChange();
-            }
-        })
+        // // working on the previous and next button of the lightBox
+        // const prev = document.querySelector('.lightBoxPrev');
+        // const next = document.querySelector('.lightBoxNext');
+        // prev.addEventListener('click', (e) => {
+        //     newIndex--;
+        //     if(newIndex < 0){
+        //         newIndex = bigImg.length-1;
+        //         lightBoxMainImage.src = bigImg[newIndex].src;
+        //         borderChange();
+        //     }
+        //     else{
+        //         lightBoxMainImage.src = bigImg[newIndex].src;
+        //         borderChange();
+        //     }
+        // })
+        // next.addEventListener('click', (e) => {
+        //     newIndex++;
+        //     if(newIndex > bigImg.length-1){
+        //         newIndex = 0;
+        //         lightBoxMainImage.src = bigImg[newIndex].src;
+        //         borderChange();
+        //     }
+        //     else{
+        //         lightBoxMainImage.src = bigImg[newIndex].src;
+        //         borderChange();
+        //     }
+        // })
     })
 }
 
@@ -181,6 +178,9 @@ const lightBoxBackground = document.querySelector('.lightBoxBackground');
 const lightBoxMainImage = document.querySelector('.lightBoxMainImage img');
 mainImage.addEventListener('click' , (e) => {
     lightBoxBackground.style.display = 'flex';
+    for(let i=0; i<img.length; i++) {
+        lightBoxMainImage.src = mainImage.src;
+    }
 })
 
 // working on the close icon
@@ -202,4 +202,98 @@ const closeList = document.querySelector('.closeList');
 closeList.addEventListener('click', (e) => {
     document.querySelector('.positionList').style.display = 'none';
     lightBoxBackground.style.display = 'none';
+})
+
+// working on the previous and next button of the mobile view
+const prev = document.querySelectorAll('.prev');
+const next = document.querySelectorAll('.next');
+
+// working on setting the corresponding border and opacity of the thumbnail images
+function borderChange(newIndex){
+    Array.from(lightBoxImg).forEach((im) => {
+        Array.from(lightBoxImage).forEach((el) => {
+            el.style.border = 'none';
+            im.style.opacity = '1';
+        })
+        im.addEventListener('mouseenter', (e) => {
+            im.style.opacity = '0.7';
+        })
+        im.addEventListener('mouseleave', (e) => {
+            im.style.opacity = '1';
+        })
+    })
+    lightBoxImage[newIndex].style.border = '2px solid hsl(26, 100%, 55%)';
+    lightBoxImg[newIndex].style.opacity = '0.4';
+    lightBoxImg[newIndex].addEventListener('mouseenter', (e) => {
+        lightBoxImg[newIndex].style.opacity = '0.4';
+    })
+    lightBoxImg[newIndex].addEventListener('mouseleave', (e) => {
+        lightBoxImg[newIndex].style.opacity = '0.4';
+    })
+}
+
+// working on the previous and next button of the lightBox
+// prev.addEventListener('click', (e) => {
+//     newIndex--;
+//     if(newIndex < 0){
+//         newIndex = bigImg.length-1;
+//         lightBoxMainImage.src = bigImg[newIndex].src;
+//         borderChange();
+//     }
+//     else{
+//         lightBoxMainImage.src = bigImg[newIndex].src;
+//         borderChange();
+//     }
+// })
+// next.addEventListener('click', (e) => {
+//     newIndex++;
+//     if(newIndex > bigImg.length-1){
+//         newIndex = 0;
+//         lightBoxMainImage.src = bigImg[newIndex].src;
+//         borderChange();
+//     }
+//     else{
+//         lightBoxMainImage.src = bigImg[newIndex].src;
+//         borderChange();
+//     }
+// })
+
+let i = 1;
+
+prev.forEach((previous) => {
+    previous.addEventListener('click', (e) => {
+        i--;
+        document.querySelector('.mobileMainImage img').src = `../images/image-product-${i}.jpg`;
+        let j = Array.from(lightBoxMainImage.src)[99];
+        j--
+        lightBoxMainImage.src = `../images/image-product-${j}.jpg`;
+        if(i < 1){
+            i = 4;
+            document.querySelector('.mobileMainImage img').src = `../images/image-product-${i}.jpg`;
+        }
+        if(j < 1){
+            j = 4;
+            lightBoxMainImage.src = `../images/image-product-${j}.jpg`;
+        }
+        borderChange(j-1);
+    })
+})
+
+next.forEach((nextt) => {
+    nextt.addEventListener('click', (e) => { 
+        i++;
+        document.querySelector('.mobileMainImage img').src = `../images/image-product-${i}.jpg`;
+        let j = Array.from(lightBoxMainImage.src)[99];
+        j++;
+        lightBoxMainImage.src = `../images/image-product-${j}.jpg`;
+        if(i > 4){
+            i = 1;
+            document.querySelector('.mobileMainImage img').src = `../images/image-product-${i}.jpg`;
+        }
+        if(j > 4){
+            j = 1;
+            lightBoxMainImage.src = `../images/image-product-${j}.jpg`;
+        }
+        borderChange(j-1);
+    })
 })
